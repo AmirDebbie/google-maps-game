5349d8bb230421d22f0d40abd8bb98fc2584f054
AmirDebbie <67858073+AmirDebbie@users.noreply.github.com>
2020-12-17 09:56:30 +0200

    Updates

---
+++ precache-manifest.e2d0c6538e11929a856454445151a441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ad944f68b402805ffec10195f63063",
+    "url": "/google-maps-game/index.html"
+  },
+  {
+    "revision": "0e9225ef06502773d718",
+    "url": "/google-maps-game/static/css/main.73bf1a45.chunk.css"
+  },
+  {
+    "revision": "6f48479b8c0448872cf0",
+    "url": "/google-maps-game/static/js/2.01bd4117.chunk.js"
+  },
+  {
+    "revision": "3a2ea33e8a131b8b33cb161570e8658e",
+    "url": "/google-maps-game/static/js/2.01bd4117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9225ef06502773d718",
+    "url": "/google-maps-game/static/js/main.e7041026.chunk.js"
+  },
+  {
+    "revision": "c3388b37e5870d7296db",
+    "url": "/google-maps-game/static/js/runtime-main.6cc2c54b.js"
+  }
+]);
